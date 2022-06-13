ba47aaff286e335248a2a9694b18b56c46caf95d
Storm Søndergaard <stormsoendergaard@gmail.com>
2022-06-13 13:00:56 +0200

    Compiles...

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,31 @@
+
+#define GOOGLE_STRIP_LOG 0
+#include <evpp/udp/udp_server.h>
+#include <evpp/udp/udp_message.h>
+
+#include <vector>
+
 int main(int argc, char** argv)
 {
-	return 0;
+    WSADATA wsaData;
+    WORD wVersionRequested = MAKEWORD(2, 2);
+    int err = WSAStartup(wVersionRequested, &wsaData);
+    if (0 != err) {
+        return 0;
+    }
+
+    std::vector<int> ports = { 1053, 5353 };
+    evpp::udp::Server server;
+
+    server.SetMessageHandler([](evpp::EventLoop* loop, evpp::udp::MessagePtr& msg) {
+        evpp::udp::SendMessage(msg);
+        });
+    server.Init(ports);
+    server.Start();
+
+    while (!server.IsStopped()) {
+        usleep(1);
+    }
+	
+    return 0;
 }
