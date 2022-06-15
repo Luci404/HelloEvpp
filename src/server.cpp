1a67c6620118d135f57c30ab558bbdc3f2824675
Storm Søndergaard <stormsoendergaard@gmail.com>
2022-06-15 16:40:08 +0200

    Address class

--- src/server.cpp
+++ src/server.cpp
@@ -30,9 +30,271 @@ public:
 
 uint16_t incrementalIdentifier = 0;
 
-#include <unordered_map>
+enum class AddressFamily
+{
+	None,
+	IPv4,
+	IPv6,
+};
+
+class NotImplementedException : public std::logic_error
+{
+public:
+	NotImplementedException() : std::logic_error("Function not yet implemented") { };
+};
+
+struct Address
+{
+	Address()
+		: type(AddressFamily::None)
+	{
+	}
+
+	Address(const sockaddr* sa)
+	{
+		memset(&address, 0, sizeof(address));
+
+		switch (sa->sa_family)
+		{
+		case AF_INET:
+		{
+			type = AddressFamily::IPv4;
+
+			const sockaddr_in* sin = reinterpret_cast<const sockaddr_in*>(sa);
+			port = sin->sin_port;
+			memcpy(&sin->sin_addr, address.ipv4, sizeof(address.ipv4));
+
+			break;
+		}
+		case AF_INET6:
+		{
+			type = AddressFamily::IPv6;
+
+			const sockaddr_in6* sin6 = reinterpret_cast<const sockaddr_in6*>(sa);
+			port = sin6->sin6_port;
+			memcpy(&sin6->sin6_addr, address.ipv6, sizeof(address.ipv6));
+		}
+		default:
+		{
+			type = AddressFamily::None;
+
+			// Error?
+			assert(false);
+		}
+		}
+	}
+
+	Address(const char* address)
+	{
+		throw NotImplementedException();
+	}
+
+	/*/Address& operator=(const Address& other)
+	{
+		x = other.x;
+		return *this;
+	}*/
+
+	bool operator==(const Address& other)
+	{
+		switch (type)
+		{
+		case AddressFamily::None:
+		{
+			// Not sure about this?
+			return type == other.type
+				&& port == other.port;
+		}
+		case AddressFamily::IPv4:
+		{
+			return type == other.type
+				&& port == other.port
+				&& address.ipv4 == other.address.ipv4;
+		}
+		case AddressFamily::IPv6:
+		{
+			return type == other.type
+				&& port == other.port
+				&& address.ipv6 == other.address.ipv6;
+		}
+		default:
+		{
+			assert(false);
+			return false;
+		}
+		}
+	}
+
+	union { uint8_t ipv4[4]; uint8_t ipv6[8]; } address;
+	uint16_t port;
+	AddressFamily type;
+};
+
+
+/*
+* https://github.com/networkprotocol/netcode/blob/master/netcode.c
+static void test_address()
+{
+	{
+		struct netcode_address_t address;
+		check(netcode_parse_address("", &address) == NETCODE_ERROR);
+		check(netcode_parse_address("[", &address) == NETCODE_ERROR);
+		check(netcode_parse_address("[]", &address) == NETCODE_ERROR);
+		check(netcode_parse_address("[]:", &address) == NETCODE_ERROR);
+		check(netcode_parse_address(":", &address) == NETCODE_ERROR);
+		check(netcode_parse_address("1", &address) == NETCODE_ERROR);
+		check(netcode_parse_address("12", &address) == NETCODE_ERROR);
+		check(netcode_parse_address("123", &address) == NETCODE_ERROR);
+		check(netcode_parse_address("1234", &address) == NETCODE_ERROR);
+		check(netcode_parse_address("1234.0.12313.0000", &address) == NETCODE_ERROR);
+		check(netcode_parse_address("1234.0.12313.0000.0.0.0.0.0", &address) == NETCODE_ERROR);
+		check(netcode_parse_address("1312313:123131:1312313:123131:1312313:123131:1312313:123131:1312313:123131:1312313:123131", &address) == NETCODE_ERROR);
+		check(netcode_parse_address(".", &address) == NETCODE_ERROR);
+		check(netcode_parse_address("..", &address) == NETCODE_ERROR);
+		check(netcode_parse_address("...", &address) == NETCODE_ERROR);
+		check(netcode_parse_address("....", &address) == NETCODE_ERROR);
+		check(netcode_parse_address(".....", &address) == NETCODE_ERROR);
+	}
+
+	{
+		struct netcode_address_t address;
+		check(netcode_parse_address("107.77.207.77", &address) == NETCODE_OK);
+		check(address.type == NETCODE_ADDRESS_IPV4);
+		check(address.port == 0);
+		check(address.data.ipv4[0] == 107);
+		check(address.data.ipv4[1] == 77);
+		check(address.data.ipv4[2] == 207);
+		check(address.data.ipv4[3] == 77);
+	}
+
+	{
+		struct netcode_address_t address;
+		check(netcode_parse_address("127.0.0.1", &address) == NETCODE_OK);
+		check(address.type == NETCODE_ADDRESS_IPV4);
+		check(address.port == 0);
+		check(address.data.ipv4[0] == 127);
+		check(address.data.ipv4[1] == 0);
+		check(address.data.ipv4[2] == 0);
+		check(address.data.ipv4[3] == 1);
+	}
+
+	{
+		struct netcode_address_t address;
+		check(netcode_parse_address("107.77.207.77:40000", &address) == NETCODE_OK);
+		check(address.type == NETCODE_ADDRESS_IPV4);
+		check(address.port == 40000);
+		check(address.data.ipv4[0] == 107);
+		check(address.data.ipv4[1] == 77);
+		check(address.data.ipv4[2] == 207);
+		check(address.data.ipv4[3] == 77);
+	}
+
+	{
+		struct netcode_address_t address;
+		check(netcode_parse_address("127.0.0.1:40000", &address) == NETCODE_OK);
+		check(address.type == NETCODE_ADDRESS_IPV4);
+		check(address.port == 40000);
+		check(address.data.ipv4[0] == 127);
+		check(address.data.ipv4[1] == 0);
+		check(address.data.ipv4[2] == 0);
+		check(address.data.ipv4[3] == 1);
+	}
+
+	{
+		struct netcode_address_t address;
+		check(netcode_parse_address("fe80::202:b3ff:fe1e:8329", &address) == NETCODE_OK);
+		check(address.type == NETCODE_ADDRESS_IPV6);
+		check(address.port == 0);
+		check(address.data.ipv6[0] == 0xfe80);
+		check(address.data.ipv6[1] == 0x0000);
+		check(address.data.ipv6[2] == 0x0000);
+		check(address.data.ipv6[3] == 0x0000);
+		check(address.data.ipv6[4] == 0x0202);
+		check(address.data.ipv6[5] == 0xb3ff);
+		check(address.data.ipv6[6] == 0xfe1e);
+		check(address.data.ipv6[7] == 0x8329);
+	}
+
+	{
+		struct netcode_address_t address;
+		check(netcode_parse_address("::", &address) == NETCODE_OK);
+		check(address.type == NETCODE_ADDRESS_IPV6);
+		check(address.port == 0);
+		check(address.data.ipv6[0] == 0x0000);
+		check(address.data.ipv6[1] == 0x0000);
+		check(address.data.ipv6[2] == 0x0000);
+		check(address.data.ipv6[3] == 0x0000);
+		check(address.data.ipv6[4] == 0x0000);
+		check(address.data.ipv6[5] == 0x0000);
+		check(address.data.ipv6[6] == 0x0000);
+		check(address.data.ipv6[7] == 0x0000);
+	}
+
+	{
+		struct netcode_address_t address;
+		check(netcode_parse_address("::1", &address) == NETCODE_OK);
+		check(address.type == NETCODE_ADDRESS_IPV6);
+		check(address.port == 0);
+		check(address.data.ipv6[0] == 0x0000);
+		check(address.data.ipv6[1] == 0x0000);
+		check(address.data.ipv6[2] == 0x0000);
+		check(address.data.ipv6[3] == 0x0000);
+		check(address.data.ipv6[4] == 0x0000);
+		check(address.data.ipv6[5] == 0x0000);
+		check(address.data.ipv6[6] == 0x0000);
+		check(address.data.ipv6[7] == 0x0001);
+	}
+
+	{
+		struct netcode_address_t address;
+		check(netcode_parse_address("[fe80::202:b3ff:fe1e:8329]:40000", &address) == NETCODE_OK);
+		check(address.type == NETCODE_ADDRESS_IPV6);
+		check(address.port == 40000);
+		check(address.data.ipv6[0] == 0xfe80);
+		check(address.data.ipv6[1] == 0x0000);
+		check(address.data.ipv6[2] == 0x0000);
+		check(address.data.ipv6[3] == 0x0000);
+		check(address.data.ipv6[4] == 0x0202);
+		check(address.data.ipv6[5] == 0xb3ff);
+		check(address.data.ipv6[6] == 0xfe1e);
+		check(address.data.ipv6[7] == 0x8329);
+	}
+
+	{
+		struct netcode_address_t address;
+		check(netcode_parse_address("[::]:40000", &address) == NETCODE_OK);
+		check(address.type == NETCODE_ADDRESS_IPV6);
+		check(address.port == 40000);
+		check(address.data.ipv6[0] == 0x0000);
+		check(address.data.ipv6[1] == 0x0000);
+		check(address.data.ipv6[2] == 0x0000);
+		check(address.data.ipv6[3] == 0x0000);
+		check(address.data.ipv6[4] == 0x0000);
+		check(address.data.ipv6[5] == 0x0000);
+		check(address.data.ipv6[6] == 0x0000);
+		check(address.data.ipv6[7] == 0x0000);
+	}
+
+	{
+		struct netcode_address_t address;
+		check(netcode_parse_address("[::1]:40000", &address) == NETCODE_OK);
+		check(address.type == NETCODE_ADDRESS_IPV6);
+		check(address.port == 40000);
+		check(address.data.ipv6[0] == 0x0000);
+		check(address.data.ipv6[1] == 0x0000);
+		check(address.data.ipv6[2] == 0x0000);
+		check(address.data.ipv6[3] == 0x0000);
+		check(address.data.ipv6[4] == 0x0000);
+		check(address.data.ipv6[5] == 0x0000);
+		check(address.data.ipv6[6] == 0x0000);
+		check(address.data.ipv6[7] == 0x0001);
+	}
+}
+*/
+
+#include <array>
 // http://www.blog.matejzavrsnik.com/map_vs_unordered_map_performance.html
-std::array<sockaddr, MAX_CLIENTS> ClientAddress;
+std::array<Address, MAX_CLIENTS> ClientAddress;
 std::array<bool, MAX_CLIENTS> ClientConnected;
 
 uint16_t FindFreeClientIdentifier()
@@ -48,11 +310,11 @@ uint16_t FindFreeClientIdentifier()
 	return InvalidIdentifier;
 }
 
-uint16_t FindClientIdentifier(const sockaddr* address)
+uint16_t FindClientIdentifier(const Address& address)
 {
 	for (int i = 0; i < MAX_CLIENTS; ++i)
 	{
-		if (ClientConnected[i] && ClientAddress[i] == *address)
+		if (ClientConnected[i] && ClientAddress[i] == address)
 		{
 			return i;
 		}
@@ -94,7 +356,7 @@ int main(int argc, char** argv)
 		// if unreliable
 		if (mode == 0)
 		{
-			std::cout << "handling unreliable package" << std::endl; 
+			std::cout << "handling unreliable package" << std::endl;
 			// Forward to packet handlers
 
 			if (clientIdentifier == InvalidIdentifier)
@@ -119,6 +381,9 @@ int main(int argc, char** argv)
 		// else if connection type
 		else if (mode == 2)
 		{
+			// WARN: This connection protocol, although fast, very unsecure. This will have to be changed before production.
+			// Read: https://gafferongames.com/post/client_server_connection/
+
 			/*
 			If the sender corresponds to the address of a client that is already connected, also reply with connection accepted.
 			This is necessary because the first response packet may not have gotten through due to packet loss. If we don’t resend this response,
@@ -127,7 +392,12 @@ int main(int argc, char** argv)
 			if (clientIdentifier == InvalidIdentifier)
 			{
 				std::cout << "client already connected" << std::endl;
+
 				// TODO: send connection accepted
+				evpp::udp::MessagePtr msg = std::make_shared<evpp::udp::Message>(msg->sockfd());
+				msg->AppendInt8(2); // mode
+				msg->AppendInt8(1); // status code
+				evpp::udp::SendMessage(msg);
 				return;
 			}
 
@@ -140,18 +410,26 @@ int main(int argc, char** argv)
 			uint16_t freeIdentifier = FindFreeClientIdentifier();
 			if (freeIdentifier != InvalidIdentifier)
 			{
-				ClientAddress[freeIdentifier] = *msg->remote_addr();
+				ClientAddress[freeIdentifier] = Address(msg->remote_addr());
 				ClientConnected[freeIdentifier] = true;
 
 				// Send connection accepted
+				evpp::udp::MessagePtr msg = std::make_shared<evpp::udp::Message>(msg->sockfd());
+				msg->AppendInt8(2); // mode
+				msg->AppendInt8(1); // status code
+				evpp::udp::SendMessage(msg);
 			}
-			
+
 			/*
 			If the server is full, reply with connection denied.
 			*/
 			else
 			{
 				// Server is full, send connection denied
+				evpp::udp::MessagePtr msg = std::make_shared<evpp::udp::Message>(msg->sockfd());
+				msg->AppendInt8(2); // mode
+				msg->AppendInt8(0); // status code
+				evpp::udp::SendMessage(msg);
 			}
 		}
 		else
